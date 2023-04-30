23330fed4c513732d4b8f1cc4537ba7429b0d294
Skript <sctr228@gmail.com>
2023-04-30 18:38:40 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -34,19 +34,25 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 80
         self.alive = True
 
     def to_study(self):
         print('Time to study')
         self.progress += 0.12
-        self.gladness -= 5
+        self.gladness -= 4
     def to_sleep(self):
         print('I will sleep')
         self.gladness += 3
     def to_chill(self):
         print('Rest time')
-        self.gladness += 5
+        self.gladness += 6
         self.progress -= 0.1
+        self.money -= 8
+    def to_work(self):
+        print('Time to study')
+        self.money += 8
+        self.gladness -= 4
     def is_alive(self):
         if self.progress < - 0.5:
             print('Cast out...')
@@ -56,19 +62,24 @@ class Student:
             self.alive = False
         elif self.progress > 5:
             print('Passed exernally...')
+        elif self.money <=0:
+            print('Bankrupt...')
     def end_of_day(self):
         print(f'Gladness = {self.gladness}')
         print(f'Progress = {round(self.progress, 2)}')
+        print(f'Money = {self.money}')
     def live(self, day):
         day = 'Day' + str(day) + 'of' + self.name + 'life'
         print(f'{day:=^50}')
-        live_cube = random.randint(1, 3)
+        live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
+        elif live_cube == 4:
+            self.to_work()
         self.end_of_day()
         self.is_alive()
 
